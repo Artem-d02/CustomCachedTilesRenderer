9d027618c0d2807b5262a9410f367f8839039dcd
Artem-d02 <artemd2002@yandex.ru>
2023-10-02 18:11:14 +0300

    Added an opportunity to measure fps

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,13 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { TilesRenderer } from './ChangedTilesRenderer/index'
-//import { TilesRenderer } from '3d-tiles-renderer'
-import { Tile } from '3d-tiles-renderer/src/base/Tile'
-import test from './MyClasses.ts/Test'
+//import { TilesRenderer } from './ChangedTilesRenderer/index'
+import { TilesRenderer } from '3d-tiles-renderer'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000)
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
@@ -16,7 +14,7 @@ document.body.appendChild(renderer.domElement)
 renderer.setClearColor(0x84cdfa, 1)
 
 console.log(__dirname)
-const tilesRenderer = new TilesRenderer('./resources/3walls_project/Tileset.json')
+const tilesRenderer = new TilesRenderer('./resources/TilesTest/Tileset.json')
 tilesRenderer.setCamera(camera)
 tilesRenderer.setResolutionFromRenderer(camera, renderer)
 tilesRenderer.onLoadModel = function (scene) {
@@ -29,11 +27,13 @@ tilesRenderer.onLoadModel = function (scene) {
         }
     })
 }
+tilesRenderer.group.rotateX((3 * Math.PI) / 2)
+
 scene.add(tilesRenderer.group)
 
 const mainLight_position = new THREE.Vector3(5, 0, 10)
 
-const mainLight = new THREE.DirectionalLight('white', 100)
+const mainLight = new THREE.DirectionalLight('white', 10)
 mainLight.position.set(mainLight_position.x, mainLight_position.y, mainLight_position.z)
 
 const light_geometry = new THREE.SphereGeometry(0.5, 32, 32)
@@ -43,20 +43,41 @@ const light_material = new THREE.MeshStandardMaterial({
 const light_mesh = new THREE.Mesh(light_geometry, light_material)
 light_mesh.position.set(mainLight_position.x, mainLight_position.y, mainLight_position.z)
 scene.add(mainLight, light_mesh)
+scene.add(mainLight.target)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+// controls
+let controls = new OrbitControls(camera, renderer.domElement)
+controls.screenSpacePanning = false
+controls.minDistance = 1
+controls.maxDistance = 100000
 
 setTimeout(() => console.log('TilesRenderer:', tilesRenderer), 0)
 setTimeout(() => console.log('TilesRenderer:', tilesRenderer), 10000)
 setTimeout(() => console.log('TilesRenderer:', tilesRenderer), 20000)
 
-//  Test for tile comparison
-let a = 'this is uri'
-let b = 'this is uri'
-console.log('a == b:', a == b)
+let fpsCounter = 0
+
+document.addEventListener('keydown', (event) => {
+    if (event.code == 'KeyM') {
+        console.log('Start measuring (10 seconds)')
+        fpsCounter = 0
+        controls.autoRotate = true
+        setTimeout(() => {
+            controls.autoRotate = false
+            console.log('fps:', fpsCounter / 10)
+        }, 10000)
+    }
+})
 
 function animate() {
     requestAnimationFrame(animate)
+
+    fpsCounter++
+    const camPos = camera.position
+    let dir = new THREE.Vector3()
+    camera.getWorldDirection(dir)
+
+    mainLight.position.set(camPos.x, camPos.y, camPos.z)
 
     controls.update()
     tilesRenderer.update()
